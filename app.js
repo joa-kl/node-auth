44e0773a64b4bca711104f6d1f04fc84be0d7a91
Joanna K <joannakleinrm@gmail.com>
2023-08-27 18:31:16 +0200

    auth middleware created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
 
 require('dotenv').config();
 
@@ -27,7 +28,7 @@ mongoose.connect(dbURI, {
 
 // routes
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 
