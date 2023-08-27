a10df028c772f99b457b82e625aa525cd165d5c7
Joanna K <joannakleinrm@gmail.com>
2023-08-27 11:44:22 +0200

    hashed jwt

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const {isEmail} = require('validator')
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     email: {
@@ -13,8 +14,20 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: [true, 'Please enter a password'],
-        minlength: [6,'Min. password length is 6 characters']
+        minlength: [6, 'Min. password length is 6 characters']
     },
+});
+
+// userSchema.post('save', function (doc, next) {
+//     console.log('new user was created and saved', doc);
+//     next();
+// });
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    // console.log(this.password)
+    next();
 })
 
 const User = mongoose.model('user', userSchema);
