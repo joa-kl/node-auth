5d48240557f5ccca2edf3783a92c3a53f995a32e
Joanna K <joannakleinrm@gmail.com>
2023-08-27 19:22:03 +0200

    authmiddleware update with checkUser

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
-const { requireAuth } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 
 require('dotenv').config();
 
@@ -27,25 +27,7 @@ mongoose.connect(dbURI, {
   .catch((err) => console.log(err));
 
 // routes
+app.get('*', checkUser);
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
-
-
-
-
-// //cookies
-// app.get('/set-cookies', (req, res) => {
-//   // res.setHeader('Set-Cookie', 'newUser=true');
-//   res.cookie('newUser', false);
-//   res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, secure: true, httpOnly: true });
-  
-//   res.send('You got the cookie!');
-// });
-
-// app.get('/read-cookies', (req, res) => {
-//   const cookies = req.cookies;
-//   console.log(cookies.newUser);
-
-//   res.json(cookies);
-// });
